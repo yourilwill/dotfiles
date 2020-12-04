1e8cafe7662dbf44a42ed1315dbff806de5027d8
yourilwill <yourilwill.n.h@gmail.com>
2020-12-04 21:55:01 +0900

    [remove] MRO proxy settings.

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,8 @@ set -U FZF_LEGACY_KEYBINDINGS 0
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 
-set --export http_proxy http://172.26.0.100:3128/
-set --export https_proxy http://172.26.0.100:3128/
+# set --export http_proxy http://172.26.0.100:3128/
+# set --export https_proxy http://172.26.0.100:3128/
 
 alias g='git'
 alias xkey='sudo -E xkeysnail config.py'
