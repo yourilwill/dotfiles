5d55c8f1c8a66a4923d2711f584128b152e3db9b
yourilwill <yourilwill.n.h@gmail.com>
2023-06-26 06:47:26 +0900

    [add] git prompt setup script.

---
+++ git_prompt_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cd || exit
+curl -o .git-completion.sh \
+    https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o .git-prompt.sh \
+    https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+echo ""
+echo "Insert these scripts to the bottom of your .bashrc."
+echo "--------------------------------------------------"
+
+cat << 'EOS'
+if [ -f ~/.git-completion.sh ]; then
+    source ~/.git-completion.sh
+fi
+if [ -f ~/.git-prompt.sh ]; then
+    source ~/.git-prompt.sh
+fi
+GIT_PS1_SHOWDIRTYSTATE=
+GIT_PS1_SHOWUNTRACKEDFILES=
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUPSTREAM=auto
+
+#PS1='[\[\033[32m\]\u@\h\[\033[00m\] \[\033[33m\]\w\[\033[1;36m\]$(__git_ps1 " (%s)")\[\033[00m\]]\n\$ '
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[1;36m\]$(__git_ps1 " (%s)")\[\033[00m\]\$ '
+EOS
+echo "--------------------------------------------------"
